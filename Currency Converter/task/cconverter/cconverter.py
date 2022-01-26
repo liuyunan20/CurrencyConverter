6d5331125dc63247447237f6b9be687c4972c7e2
liuyunan <liuyunan.20@gmail.com>
2022-01-26 09:05:52 -0800

    Initial commit

---
+++ Currency Converter/task/cconverter/cconverter.py
@@ -0,0 +1,12 @@
+conicoin = float(input())
+rub = conicoin * 2.98
+ars = conicoin * 0.82
+hnl = conicoin * 0.17
+aud = conicoin * 1.9622
+mad = conicoin * 0.208
+print(f'''I will get {rub} RUB from the sale of {conicoin} conicoins.
+I will get {ars} ARS from the sale of {conicoin} conicoins.
+I will get {hnl} HNL from the sale of {conicoin} conicoins.
+I will get {aud} AUD from the sale of {conicoin} conicoins.
+I will get {mad} MAD from the sale of {conicoin} conicoins.
+''')
