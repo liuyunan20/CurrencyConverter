d1de90af678466d2197e71c213caa1238a896de0
liuyunan <liuyunan.20@gmail.com>
2022-01-26 16:48:35 -0800

    find specific currency rate by code from website

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,12 +1,8 @@
-conicoin = float(input())
-rub = conicoin * 2.98
-ars = conicoin * 0.82
-hnl = conicoin * 0.17
-aud = conicoin * 1.9622
-mad = conicoin * 0.208
-print(f'''I will get {rub} RUB from the sale of {conicoin} conicoins.
-I will get {ars} ARS from the sale of {conicoin} conicoins.
-I will get {hnl} HNL from the sale of {conicoin} conicoins.
-I will get {aud} AUD from the sale of {conicoin} conicoins.
-I will get {mad} MAD from the sale of {conicoin} conicoins.
-''')
+import requests
+import json
+
+currency = input().lower()
+r = requests.get(f"http://www.floatrates.com/daily/{currency}.json")
+rates = json.loads(r.content)
+print(rates["usd"])
+print(rates["eur"])
