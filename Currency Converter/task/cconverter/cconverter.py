c670630ab08eca3fdfbfe8c3b906be99d1ba84f2
liuyunan <liuyunan.20@gmail.com>
2022-01-26 17:48:16 -0800

    find specific currency rate by code from website and calculate the amount can be received

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,8 +1,26 @@
 import requests
 import json
 
-currency = input().lower()
-r = requests.get(f"http://www.floatrates.com/daily/{currency}.json")
+my_currency = input().lower()
+r = requests.get(f"http://www.floatrates.com/daily/{my_currency}.json")
 rates = json.loads(r.content)
-print(rates["usd"])
-print(rates["eur"])
+if my_currency == "usd":
+    ex_rate = {"eur": rates["eur"]["rate"]}
+elif my_currency == "eur":
+    ex_rate = {"usd": rates["usd"]["rate"]}
+else:
+    ex_rate = {"usd": rates["usd"]["rate"], "eur": rates["eur"]["rate"]}
+while True:
+    target_currency = input().lower()
+    if target_currency:
+        amount = float(input())
+        print("Checking the cache...")
+        if target_currency not in ex_rate:
+            ex_rate[target_currency] = rates[target_currency]["rate"]
+            print("Sorry, but it is not in the cache!")
+        else:
+            print("Oh! It is in the cache!")
+        received_amount = round(amount * ex_rate[target_currency], 2)
+        print(f"You received {received_amount} {target_currency.upper()}.")
+    else:
+        break
